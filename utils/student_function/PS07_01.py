7c7bcb5ec12848f5acb594ac58293d6352e815d9
Daniel Rivera <danielf.r97@gmail.com>
2018-09-26 22:15:58 -0500

    Listo el 7.1

--- utils/student_function/PS07_01.py
+++ utils/student_function/PS07_01.py
@@ -2,21 +2,23 @@ import PS07
 class BTNode(PS07.BTNode):
     
     def height(self):
-        if self is None:return 0
         alturaIzquierda=0;alturaDerecha=0
-        if self.left is not None:alturaIzquierda=self.left.height()
-        if self.right is not None:alturaDerecha=self.right.height()
-        if alturaIzquierda>alturaDerecha:return alturaIzquierda+1
-        else: return alturaDerecha+1
+        if((self.left is None) and (self.right is None)):return 0
+        if self.left is not None:
+            alturaIzquierda=self.left.height()+1
+        if self.right is not None:
+            alturaDerecha=self.right.height()+1
+        if alturaIzquierda>alturaDerecha:return alturaIzquierda
+        else: return alturaDerecha
     
-   
     def balance_factor(self):
+        a=0;b=0
+        if self.left is not None:
+            a=self.left.height()+1
+        if self.right is not None:
+            b=self.right.height()+1
+        result=(a-b)
         
-    
-        a=self.left.height()
-        
-        b=self.right.height()
-        result=(a-b)   
         return result
     
     def balance_factor_tree(self):
@@ -24,4 +26,4 @@ class BTNode(PS07.BTNode):
                            left=self.left.balance_factor_tree() if self.left is not None else None,
                            right=self.right.balance_factor_tree() if self.right is not None else None,
                           )
-        return r     
+        return r
